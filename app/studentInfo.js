88d6a3230f3f12a671ca6d83e233fdc9d66e46f6
Prit Patel <119982041+prit1543@users.noreply.github.com>
2023-09-20 14:21:35 -0600

    week2_edit root page

--- app/studentInfo.js
+++ app/studentInfo.js
@@ -7,7 +7,7 @@ export default function StudentInfo()
         <div>
             <h1>Student Information</h1> 
             <p>Student Name: Prit Patel<br />
-               course: CPRG 306 A<br />
+               Course: CPRG 306 A<br />
                <Link href="https://github.com/prit-1543/cprg306-assignments.git">Github Link</Link>
             </p>         
         </div>
