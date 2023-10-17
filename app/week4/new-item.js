02ef01368ef7693c9acf432d20d42ae42c767e67
Prit Patel <119982041+prit1543@users.noreply.github.com>
2023-10-16 20:11:26 -0600

    final without css

--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -1,84 +1,60 @@
 "use client";
-import React, { useState } from 'react';
 
+import { useState } from 'react';
 
-function NewItem() {
-  // Initialize state variables
-  const [name, setName] = useState("");
-  const [quantity, setQuantity] = useState(1);
-  const [category, setCategory] = useState("produce");
+const NewItem = () => {
+    const [name, setName] = useState("");
+    const [quantity, setQuantity] = useState(1);
+    const [category, setCategory] = useState("produce");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const item = {
-      name,
-      quantity,
-      category
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        
+        const item = {
+            name,
+            quantity,
+            category,
+        };
+        console.log(item);
+        alert(`Name: ${name}, Quantity: ${quantity}, Category: ${category}`);
+        
+        // Reset state variables
+        setName("");
+        setQuantity(1);
+        setCategory("produce");
     };
 
-    console.log(item);
-    alert(`Name: ${name}, Quantity: ${quantity}, Category: ${category}`);
-
-    // Reset state variables to their initial values
-    setName("");
-    setQuantity(1);
-    setCategory("produce");
-  };
-
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <form onSubmit={handleSubmit} className="p-6 bg-white rounded shadow-md w-96">
-        <div className="mb-4">
-          <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-            className="mt-1 p-2 w-full border rounded-md" />
-        </div>
-
-        <div className="mb-4">
-          <label htmlFor="quantity" className="block text-sm font-medium text-gray-700">Quantity</label>
-          <input
-            type="number"
-            id="quantity"
-            min="1"
-            max="99"
-            value={quantity}
-            onChange={(e) => setQuantity(Number(e.target.value))}
-            required
-            className="mt-1 p-2 w-full border rounded-md" />
-        </div>
-
-        <div className="mb-4">
-          <label htmlFor="category" className="block text-sm font-medium text-gray-700">Category</label>
-          <select
-            id="category"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-            className="mt-1 p-2 w-full border rounded-md"
-          >
-            <option value="produce">Produce</option>
-            <option value="dairy">Dairy</option>
-            <option value="bakery">Bakery</option>
-            <option value="meat">Meat</option>
-            <option value="frozen">Frozen Foods</option>
-            <option value="canned">Canned Goods</option>
-            <option value="dry">Dry Goods</option>
-            <option value="beverages">Beverages</option>
-            <option value="snacks">Snacks</option>
-            <option value="household">Household</option>
-            <option value="other">Other</option>
-          </select>
-        </div>
-
-        <button type="submit" className="px-4 py-2 mt-4 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">Submit</button>
-      </form>
-    </div>
-  );
+    return (
+        <form onSubmit={handleSubmit} className="bg-gray-100 p-4 rounded-lg shadow-md text-black">
+            <div className="mb-4">
+                <label className="block text-sm font-bold mb-2" htmlFor="name">Name:</label>
+                <input type="text" id="name" value={name} onChange={e => setName(e.target.value)} required className="w-full px-3 py-2 border rounded-md text-black" />
+            </div>
+            <div className="mb-4">
+                <label className="block text-sm font-bold mb-2" htmlFor="quantity">Quantity:</label>
+                <input type="number" id="quantity" min="1" max="99" value={quantity} onChange={e => setQuantity(Number(e.target.value))} required className="w-full px-3 py-2 border rounded-md text-black" />
+            </div>
+            <div className="mb-4">
+                <label className="block text-sm font-bold mb-2" htmlFor="category">Category:</label>
+                <select id="category" value={category} onChange={e => setCategory(e.target.value)} className="w-full px-3 py-2 border rounded-md text-black">
+                    <option value="produce">Produce</option>
+                    <option value="dairy">Dairy</option>
+                    <option value="bakery">Bakery</option>
+                    <option value="meat">Meat</option>
+                    <option value="frozen">Frozen Foods</option>
+                    <option value="canned">Canned Goods</option>
+                    <option value="dry">Dry Goods</option>
+                    <option value="beverages">Beverages</option>
+                    <option value="snacks">Snacks</option>
+                    <option value="household">Household</option>
+                    <option value="other">Other</option>
+                </select>
+            </div>
+            <div>
+                <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md">Submit</button>
+            </div>
+        </form>
+    );
 }
 
 export default NewItem;
--- app/week4/page.js
+++ app/week4/page.js
@@ -1,30 +1,12 @@
-// @client
-import React, {useState} from 'react';
+import NewItem from './new-item';
 
-const NewItem = () => {
-  // ... [rest of the component logic remains unchanged]
-
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <form onSubmit={handleSubmit} className="p-6 bg-white rounded shadow-md w-96">
-        <div className="mb-4">
-          <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-            className="mt-1 p-2 w-full border rounded-md"
-          />
+const Page = () => {
+    return (
+        <div className="container mx-auto mt-10 text-black">
+            <h1 className="text-2xl mb-5">Add New Item</h1>
+            <NewItem />
         </div>
-
-        {/* ... [rest of the form remains unchanged] */}
-
-        <button type="submit" className="px-4 py-2 mt-4 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">Submit</button>
-      </form>
-    </div>
-  );
+    );
 }
 
-export default NewItem;
+export default Page;
