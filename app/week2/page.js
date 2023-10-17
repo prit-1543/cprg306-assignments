6b950c8d66db7993a64cc1674d2d74435a666595
Prit Patel <119982041+prit1543@users.noreply.github.com>
2023-10-16 21:45:52 -0600

    .

--- app/week2/page.js
+++ app/week2/page.js
@@ -1,16 +1,14 @@
 import StudentInfo from "../studentInfo";
-import Link from "next/link";
 
-export default function Hello(){
-  return(
-      <main className="bg-black min-h-screen p-8">
-      <div className="max-w-2xl mx-auto">
-        <h1 className="text-4xl font-bold mb-6">My Shopping List</h1>
-        <div className="p-4 bg-gray shadow rounded">
-        <h1>My Shopping List</h1>
-        <StudentInfo />
-        </div>
-      </div>
-    </main> 
-    )
+export default function Hello() {
+    return (
+        <main className="bg-black min-h-screen p-8">
+            <div className="max-w-4xl mx-auto flex justify-between items-center space-x-12">
+                <h1 className="text-5xl font-bold text-white">My Shopping List</h1>
+                <div className="font-mono text-lg">
+                    <StudentInfo />
+                </div>
+            </div>
+        </main>
+    );
 }
