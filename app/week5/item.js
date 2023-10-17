146df132e7622e0de7198d2b2aa3737a65b481ec
Prit Patel <119982041+prit1543@users.noreply.github.com>
2023-10-16 22:59:57 -0600

    css added to page.js and item.js

--- app/week5/item.js
+++ app/week5/item.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const Item = ({ name, quantity, category }) => (
-  <li>
-    {name} - {quantity} ({category})
-  </li>
+  <li className="p-4 bg-gray-700 border rounded-md shadow-md flex justify-between items-center">
+  <div>
+    <span className="font-medium text-lg">{name}</span> 
+    <span className="text-sm ml-2 text-gray-300">({category})</span>
+  </div>
+  <span className="text-blue-300">{quantity}</span>
+</li>
 );
 
 export default Item;
--- app/week5/page.js
+++ app/week5/page.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ItemList from './item-list';
 
 const Page = () => (
-  <main>
-    <h1>Shopping List</h1>
+  <main className="p-8 bg-gray-900 min-h-screen flex flex-col items-center justify-center">
+    <h1 className="text-3xl font-semibold mb-6">Shopping List</h1>
     <ItemList />
   </main>
 );
