1562d69c4076e700b4c417c15f504b3a3b8b9410
Prit Patel <119982041+prit1543@users.noreply.github.com>
2023-10-18 16:10:06 -0600

    Update item-list.js

--- app/week5/item-list.js
+++ app/week5/item-list.js
@@ -25,31 +25,34 @@ const ItemList = () => {
   }
 
   return (
-    <div>
-      <button
-        className={sortBy === 'name' ? 'bg-blue-500' : ''}
-        onClick={() => setSortBy('name')}
-      >
-        Sort by Name
-      </button>
-      <button
-        className={sortBy === 'category' ? 'bg-blue-500' : ''}
-        onClick={() => setSortBy('category')}
-      >
-        Sort by Category
-      </button>
-      <button
-        className={sortBy === 'grouped' ? 'bg-blue-500' : ''}
-        onClick={() => setSortBy('grouped')}
-      >
-        Group by Category
-      </button>
+    <div className="bg-gray-800 p-6 text-white w-full min-h-screen">
+      <h1 className="text-3xl mb-4">Shopping List</h1>
+      <div className="flex mb-4 space-x-4">
+        <button
+          className={`px-4 py-2 rounded ${sortBy === 'name' ? 'bg-blue-500' : 'bg-gray-700'}`}
+          onClick={() => setSortBy('name')}
+        >
+          Sort by Name
+        </button>
+        <button
+          className={`px-4 py-2 rounded ${sortBy === 'category' ? 'bg-blue-500' : 'bg-gray-700'}`}
+          onClick={() => setSortBy('category')}
+        >
+          Sort by Category
+        </button>
+        <button
+          className={`px-4 py-2 rounded ${sortBy === 'grouped' ? 'bg-blue-500' : 'bg-gray-700'}`}
+          onClick={() => setSortBy('grouped')}
+        >
+          Group by Category
+        </button>
+      </div>
 
-      <ul>
+      <ul className="divide-y divide-gray-600">
         {sortBy === 'grouped'
           ? Object.keys(groupedItems).sort().map(category => (
-              <li key={category}>
-                <strong className="capitalize">{category}</strong>
+              <li key={category} className="py-4">
+                <strong className="text-xl capitalize mb-2 block">{category}</strong>
                 <ul>
                   {groupedItems[category].map(item => (
                     <Item key={item.id} {...item} />
@@ -58,7 +61,9 @@ const ItemList = () => {
               </li>
             ))
           : items.map(item => (
-              <Item key={item.id} {...item} />
+              <li key={item.id} className="py-4">
+                <Item {...item} />
+              </li>
             ))}
       </ul>
     </div>
